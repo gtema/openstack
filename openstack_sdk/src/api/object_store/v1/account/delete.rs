a3f540d4cd18ec4ebd50d27a79343de0cabe2072
gtema <artem.goncharov@gmail.com>
2024-02-01 14:28:36 +0100

    chore: add license headers

--- openstack_sdk/src/api/object_store/v1/account/delete.rs
+++ openstack_sdk/src/api/object_store/v1/account/delete.rs
@@ -1,3 +1,17 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 //! Deletes the specified account when a reseller admin issues this request.
 //! Accounts are only deleted by (1) having a reseller admin level auth token
 //! (2) sending a DELETE to a proxy server for the account to be deleted and
