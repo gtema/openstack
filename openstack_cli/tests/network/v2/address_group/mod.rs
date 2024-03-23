b90fdf0272cb7675c542f2dd9ec67b2c4e2f7dba
Artem Goncharov <artem.goncharov@gmail.com>
2024-03-23 18:50:36 +0100

    feat(network): add network address_{group,scope} resources

--- openstack_cli/tests/network/v2/address_group/mod.rs
+++ openstack_cli/tests/network/v2/address_group/mod.rs
@@ -12,13 +12,23 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-//! `Network` Service bindings
-pub mod address_group;
-pub mod address_scope;
-pub mod availability_zone;
-pub mod extension;
-pub mod floatingip;
-pub mod network;
-pub mod port;
-pub mod router;
-pub mod subnet;
+mod add_addresses_autogen;
+mod create_autogen;
+mod delete_autogen;
+mod list_autogen;
+mod remove_addresses_autogen;
+mod set_autogen;
+mod show_autogen;
+
+use assert_cmd::prelude::*;
+use std::process::Command;
+
+#[test]
+fn help() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("osc")?;
+
+    cmd.arg("network").arg("address-group").arg("--help");
+    cmd.assert().success();
+
+    Ok(())
+}
