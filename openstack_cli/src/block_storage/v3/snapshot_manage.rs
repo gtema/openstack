bec55f080699892caadaaead959338b851eda55a
Artem Goncharov <artem.goncharov@gmail.com>
2024-04-26 12:16:18 +0200

    feat: Add volume.manageable_XXX resources
    - cover cinder manageable_volume extension
    - cover cinder manageable_snapshot extension

---
+++ openstack_cli/src/block_storage/v3/snapshot_manage.rs
@@ -0,0 +1,57 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+//! Block storage Manageable Snapshot commands
+//!
+
+use clap::{Parser, Subcommand};
+
+use crate::{Cli, OpenStackCliError};
+
+use openstack_sdk::AsyncOpenStack;
+
+mod create;
+mod list;
+
+/// SnapshotManage manage extension (manageable_snapshots)
+///
+/// Creates or lists snapshots by using existing storage instead of allocating new storage.
+#[derive(Parser)]
+pub struct SnapshotManageCommand {
+    /// subcommand
+    #[command(subcommand)]
+    command: SnapshotManageCommands,
+}
+
+/// Supported subcommands
+#[allow(missing_docs)]
+#[derive(Subcommand)]
+pub enum SnapshotManageCommands {
+    Create(create::SnapshotManageCommand),
+    List(list::SnapshotManagesCommand),
+}
+
+impl SnapshotManageCommand {
+    /// Perform command action
+    pub async fn take_action(
+        &self,
+        parsed_args: &Cli,
+        session: &mut AsyncOpenStack,
+    ) -> Result<(), OpenStackCliError> {
+        match &self.command {
+            SnapshotManageCommands::Create(cmd) => cmd.take_action(parsed_args, session).await,
+            SnapshotManageCommands::List(cmd) => cmd.take_action(parsed_args, session).await,
+        }
+    }
+}
--- openstack_cli/tests/block_storage/v3/snapshot_manage/mod.rs
+++ openstack_cli/tests/block_storage/v3/snapshot_manage/mod.rs
@@ -12,22 +12,8 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-mod attachment;
-mod backup;
-mod default_type;
-mod extension;
-mod group;
-mod group_snapshot;
-mod group_type;
-mod host;
-mod limit;
-mod message;
-mod resource_filter;
-mod snapshot;
-mod snapshot_manage;
-mod r#type;
-mod volume;
-mod volume_manage;
+mod create_autogen;
+mod list_autogen;
 
 use assert_cmd::prelude::*;
 use std::process::Command;
@@ -36,7 +22,9 @@ use std::process::Command;
 fn help() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("osc")?;
 
-    cmd.arg("block-storage").arg("--help");
+    cmd.arg("block-storage")
+        .arg("snapshot-manage")
+        .arg("--help");
     cmd.assert().success();
 
     Ok(())
