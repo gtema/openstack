2b82ebfd35a20f303d2207306a72b01babcf796d
Artem Goncharov <artem.goncharov@gmail.com>
2024-06-06 18:36:27 +0200

    feat(sdk): Add possibility to skip version discovery
    According to the formal service catalog consumption process it should be
    possible to skip service discovery. Add default to skip discovery for
    object-store service and process configuration options
    (XXX_skip_discovery: bool).

--- openstack_sdk/src/openstack_async.rs
+++ openstack_sdk/src/openstack_async.rs
@@ -430,7 +430,7 @@ where {
             self.catalog
                 .get_service_endpoint(service_type.to_string(), None, None::<String>)
         {
-            if true {
+            if self.catalog.discovery_allowed(service_type.to_string()) {
                 info!("Performing `{}` endpoint version discovery", service_type);
 
                 let mut try_url = ep.url().clone();
