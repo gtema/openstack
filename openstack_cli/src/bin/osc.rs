5421dbf3ecbb61c86c3b8904301bc194ac5bf9b1
Artem Goncharov <Artem.goncharov@gmail.com>
2024-08-19 13:39:56 +0200

    feat: Introduce use of human-panic (#462)
    The rust-cli WG has a crate that is also recommended by the ratatui for
    making crash reports more human friendly. Start using it.

--- openstack_cli/src/bin/osc.rs
+++ openstack_cli/src/bin/osc.rs
@@ -19,27 +19,57 @@
 
 use color_eyre::eyre::{Report, Result};
 
-use openstack_cli::error::OpenStackCliError;
-
 #[tokio::main]
 async fn main() -> Result<(), Report> {
-    color_eyre::config::HookBuilder::default()
-        .issue_url(concat!(env!("CARGO_PKG_REPOSITORY"), "/issues/new"))
-        .add_issue_metadata("version", env!("CARGO_PKG_VERSION"))
-        .issue_filter(|kind| match kind {
-            color_eyre::ErrorKind::NonRecoverable(_) => true,
-            color_eyre::ErrorKind::Recoverable(error) => {
-                match error.downcast_ref::<OpenStackCliError>() {
-                    Some(OpenStackCliError::OpenStackApi { .. }) => false,
-                    Some(OpenStackCliError::Auth { .. }) => false,
-                    Some(OpenStackCliError::ReScope { .. }) => false,
-                    Some(OpenStackCliError::ConnectionNotFound { .. }) => false,
-                    _ => true,
-                }
-            }
-        })
-        .install()?;
-
+    initialize_panic_handler()?;
     openstack_cli::entry_point().await?;
+    Ok(())
+}
+
+/// Initialize panic handling
+fn initialize_panic_handler() -> Result<()> {
+    let (panic_hook, eyre_hook) = color_eyre::config::HookBuilder::default()
+        .panic_section(format!(
+            "This is a bug. Consider reporting it at {}/issues",
+            env!("CARGO_PKG_REPOSITORY")
+        ))
+        .capture_span_trace_by_default(false)
+        .display_location_section(false)
+        .display_env_section(false)
+        .into_hooks();
+    eyre_hook.install()?;
+    std::panic::set_hook(Box::new(move |panic_info| {
+        #[cfg(not(debug_assertions))]
+        {
+            use human_panic::{handle_dump, print_msg, Metadata};
+            let meta = Metadata::new(env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))
+                .authors(env!("CARGO_PKG_AUTHORS"))
+                .homepage(env!("CARGO_PKG_HOMEPAGE"))
+                .support(format!(
+                    "Report issue at {}/issues",
+                    env!("CARGO_PKG_HOMEPAGE")
+                ));
+
+            let file_path = handle_dump(&meta, panic_info);
+            // prints human-panic message
+            print_msg(file_path, &meta)
+                .expect("human-panic: printing error message to console failed");
+            eprintln!("{}", panic_hook.panic_report(panic_info)); // prints color-eyre stack trace to stderr
+        }
+        let msg = format!("{}", panic_hook.panic_report(panic_info));
+        tracing::error!("Error: {}", strip_ansi_escapes::strip_str(msg));
+
+        #[cfg(debug_assertions)]
+        {
+            // Better Panic stacktrace that is only enabled when debugging.
+            better_panic::Settings::auto()
+                .most_recent_first(false)
+                .lineno_suffix(true)
+                .verbosity(better_panic::Verbosity::Full)
+                .create_panic_handler()(panic_info);
+        }
+
+        std::process::exit(1);
+    }));
     Ok(())
 }
