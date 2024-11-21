5b943847c2b6de0d5f08eb06ccabbfd0bbf1d4b9
Artem Goncharov <Artem.goncharov@gmail.com>
2024-11-21 11:02:52 +0100

    chore: Update the header component (#780)
    rework header component to handle as many mode keybindings as there are.
    Collect all possible data as early as possible to reduce amount of
    operations during rendering.

--- openstack_tui/src/components/header.rs
+++ openstack_tui/src/components/header.rs
@@ -12,9 +12,14 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+//! TUI header component
+
 use eyre::Result;
 use itertools::Itertools;
 use ratatui::{prelude::*, widgets::*};
+use std::cmp::max;
+//use ratatui::{layout::Flex, prelude::*, widgets::*};
+use tracing::debug;
 
 use crate::{
     action::Action,
@@ -32,6 +37,13 @@ pub struct Header {
     cloud_name: String,
     project_name: String,
     domain_name: String,
+    /// Connection data
+    connection_data_rows: Vec<(String, String)>,
+    /// Global bindings rows
+    global_bindings_rows: Vec<(String, String)>,
+    /// Keybindings of the current mode
+    mode_keybindings: Vec<(String, String)>,
+    size: Size,
 }
 
 impl Default for Header {
@@ -40,33 +52,23 @@ impl Default for Header {
     }
 }
 
-/// Ensure string is smaller than the length otherwise truncate it adding ".." as a suffix to
-/// indicate truncation has happened
-fn ensure_max_length(value: &str, max_length: usize) -> String {
-    if value.len() <= max_length {
-        value.to_owned()
-    } else if max_length == 0 {
-        String::from("")
-    } else if max_length == 1 {
-        return String::from(".");
-    } else if max_length == 2 {
-        return format!("{}.", value.get(0..1).unwrap_or("."));
-    } else {
-        let mut result = value.to_owned();
-        result.replace_range(max_length - 2.., "..");
-        return result;
-    }
-}
-
 impl Header {
     pub fn new() -> Self {
         Self {
             config: Config::default(),
             current_mode: Mode::default(),
+            size: Size::new(0, 0),
 
             cloud_name: String::new(),
             project_name: String::new(),
             domain_name: String::new(),
+            mode_keybindings: Vec::new(),
+            connection_data_rows: Vec::new(),
+            global_bindings_rows: vec![
+                (String::from("<:>"), String::from("Select resource")),
+                (String::from("<F2>"), String::from("Switch cloud")),
+                (String::from("<F4>"), String::from("Switch project")),
+            ],
         }
     }
 
@@ -91,24 +93,59 @@ impl Component for Header {
             Action::Tick => self.app_tick()?,
             Action::Render => self.render_tick()?,
             Action::ConnectToCloud(ref cloud) => {
+                // Save information about reconnecting to the cloud
                 self.cloud_name = cloud.clone();
                 self.project_name.clear();
                 self.domain_name.clear();
+                self.connection_data_rows.clear();
+                self.connection_data_rows
+                    .extend([(String::from("Cloud:"), self.cloud_name.clone())]);
             }
-            Action::Mode(mode) => self.current_mode = mode,
+            Action::Mode(mode) => {
+                self.current_mode = mode;
+                if let Some(keymap) = self.config.mode_keybindings.get(&self.current_mode) {
+                    // Update mode keybindings rows with the current mode bindings
+                    self.mode_keybindings = keymap
+                        .iter()
+                        .map(|(k, v)| {
+                            (
+                                format!(
+                                    "<{}>",
+                                    k.iter()
+                                        .map(key_event_to_string_with_unicode)
+                                        .collect::<Vec<_>>()
+                                        .join("")
+                                ),
+                                v.description.clone().unwrap_or(String::from("")),
+                            )
+                        })
+                        .sorted()
+                        .collect();
+                }
+            }
             Action::ConnectedToCloud(ref auth_token) => {
+                // Update information about cloud we are connected to
+                self.connection_data_rows.clear();
+                self.connection_data_rows
+                    .push((String::from("Cloud:"), self.cloud_name.clone()));
                 if let Some(project) = &auth_token.project {
-                    if let Some(name) = &project.name {
-                        self.project_name = name.clone()
-                    }
                     if let Some(domain) = &project.domain {
                         if let Some(name) = &domain.name {
                             self.domain_name = name.clone();
+                            self.connection_data_rows
+                                .push((String::from("Domain:"), self.domain_name.clone()));
                         }
+                    }
+                    if let Some(name) = &project.name {
+                        self.project_name = name.clone();
+                        self.connection_data_rows
+                            .push((String::from("Project:"), self.project_name.clone()));
                     }
                 } else if let Some(domain) = &auth_token.domain {
                     if let Some(name) = &domain.name {
                         self.domain_name = name.clone();
+                        self.connection_data_rows
+                            .push((String::from("Domain:"), self.domain_name.clone()));
                     }
                 }
             }
@@ -118,86 +155,102 @@ impl Component for Header {
     }
 
     fn draw(&mut self, f: &mut Frame<'_>, rect: Rect) -> Result<(), TuiError> {
+        debug!("Header size is {:?}", rect.as_size());
+        self.size = rect.as_size();
+        // Split whole area first into 3 columns
         let rects = Layout::default()
             .direction(Direction::Horizontal)
             .constraints(vec![
-                Constraint::Min(40), // first column
-                Constraint::Min(40), // second column
-                Constraint::Min(40), // second column
-                Constraint::Percentage(100),
+                Constraint::Min(40),         // Connection stats
+                Constraint::Length(25),      // Global keybindings
+                Constraint::Percentage(100), // Mode keybindings
             ])
+            .spacing(2)
             .split(rect);
 
-        let connect_info_rows = [
-            Row::new(vec![
-                Span::styled("Cloud:", Style::new().yellow()),
-                Span::from(ensure_max_length(&self.cloud_name, 30)),
-            ]),
-            Row::new(vec![
-                Span::styled("Domain:", Style::new().yellow()),
-                Span::from(ensure_max_length(&self.domain_name, 30)),
-            ]),
-            Row::new(vec![
-                Span::styled("Project:", Style::new().yellow()),
-                Span::from(ensure_max_length(&self.project_name, 30)),
-            ]),
-        ];
-        // Columns widths are constrained in the same way as Layout...
-        let widths = [Constraint::Length(8), Constraint::Length(30)];
-        let widths2 = [Constraint::Min(4), Constraint::Fill(35)];
+        // Connection info column
+        let (c1, c2): (Vec<ListItem>, Vec<ListItem>) = self
+            .connection_data_rows
+            .iter()
+            .map(|x| (x.0.clone().into(), x.1.clone().into()))
+            .collect();
 
-        let connect_info_table = Table::new(connect_info_rows, widths)
-            .column_spacing(1)
-            .style(Style::new().white());
+        let c1_list = List::default()
+            .items(c1.clone())
+            .style(Style::new().fg(Color::Yellow));
+        let c2_list = List::default().items(c2.clone()).style(Style::new());
 
-        f.render_widget(connect_info_table.clone(), rects[0]);
+        let cols = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Length(5), Constraint::Percentage(100)])
+            .spacing(2)
+            .split(rects[0]);
+        f.render_widget(c1_list, cols[0]);
+        f.render_widget(c2_list, cols[1]);
 
-        let global_shortcuts_rows = [
-            Row::new(vec![
-                Span::styled("<:>", Style::new().yellow()),
-                Span::from("Select resource"),
-            ]),
-            Row::new(vec![
-                Span::styled("<F2>", Style::new().yellow()),
-                Span::from("Switch cloud"),
-            ]),
-            Row::new(vec![
-                Span::styled("<F4>", Style::new().yellow()),
-                Span::from("Select project scope"),
-            ]),
-        ];
-        let global_shortcuts_table = Table::new(global_shortcuts_rows, widths2)
-            .column_spacing(1)
-            .style(Style::new().white());
+        // Global keybindings column
+        let (c1, c2): (Vec<ListItem>, Vec<ListItem>) = self
+            .global_bindings_rows
+            .iter()
+            .map(|x| (x.0.clone().into(), x.1.clone().into()))
+            .collect();
 
-        f.render_widget(global_shortcuts_table.clone(), rects[1]);
+        let c1_list = List::default()
+            .items(c1.clone())
+            .style(Style::new().fg(Color::Red));
+        let c2_list = List::default().items(c2.clone()).style(Style::new());
 
-        if let Some(keymap) = self.config.mode_keybindings.get(&self.current_mode) {
-            let mut shortcuts_rows: Vec<Row> = Vec::new();
-            for (k, v) in keymap
-                .iter()
-                // Map data to be able to sort by string representation
-                .map(|(k, v)| {
-                    (
-                        k.iter()
-                            .map(key_event_to_string_with_unicode)
-                            .collect::<Vec<_>>()
-                            .join(""),
-                        v.description.clone().unwrap_or(String::from("")),
-                    )
-                })
-                .sorted()
-            {
-                shortcuts_rows.push(Row::new(vec![
-                    Span::styled(format!("<{}>", k), Style::new().yellow()),
-                    v.into(),
-                ]));
+        let cols = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![Constraint::Length(4), Constraint::Percentage(100)])
+            .spacing(2)
+            .split(rects[1]);
+        f.render_widget(c1_list, cols[0]);
+        f.render_widget(c2_list, cols[1]);
+
+        let mut remainder = rects[2];
+
+        // Mode keybindings
+        if !self.mode_keybindings.is_empty() {
+            // Count number of rows (-1 to keep some spacing)
+            let count_rows: usize = (self.size.height - 1).into();
+
+            // Iterate in chunks of the rows count
+            for col_bindings in &self.mode_keybindings.iter().chunks(count_rows) {
+                let mut bindings_width: usize = 0;
+                let mut descriptions_width: usize = 0;
+                // Collect data into rows simultaneously counting max widths
+                let (bindings, descriptions): (Vec<ListItem>, Vec<ListItem>) = col_bindings
+                    .map(|x| (x.0.clone().into(), x.1.clone().into()))
+                    .map(|(k, d): (ListItem, ListItem)| {
+                        bindings_width = max(bindings_width, k.width());
+                        descriptions_width = max(descriptions_width, d.width());
+                        (k, d)
+                    })
+                    .collect();
+
+                let bindings_list = List::default()
+                    .items(bindings.clone())
+                    .style(Style::new().fg(Color::LightBlue));
+                let descriptions_list = List::default()
+                    .items(descriptions.clone())
+                    .style(Style::new());
+
+                // Split current area into 3 (binding, description, remainder)
+                let cols = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints(vec![
+                        Constraint::Length(bindings_width as u16),
+                        Constraint::Length(descriptions_width as u16),
+                        Constraint::Percentage(100),
+                    ])
+                    .spacing(2)
+                    .split(remainder);
+                f.render_widget(bindings_list, cols[0]);
+                f.render_widget(descriptions_list, cols[1]);
+                // Update the remainder pointer
+                remainder = cols[2];
             }
-            let shortcuts_table = Table::new(shortcuts_rows, widths2)
-                .column_spacing(1)
-                .style(Style::new().white());
-
-            f.render_widget(shortcuts_table.clone(), rects[2]);
         }
 
         Ok(())
@@ -205,23 +258,4 @@ impl Component for Header {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_ensure_max_length() {
-        let original = String::from("123456789");
-        assert_eq!(String::from(""), ensure_max_length(&original, 0));
-        assert_eq!(String::from("."), ensure_max_length(&original, 1));
-        assert_eq!(String::from("1."), ensure_max_length(&original, 2));
-        assert_eq!(String::from("1.."), ensure_max_length(&original, 3));
-        assert_eq!(String::from("12.."), ensure_max_length(&original, 4));
-        assert_eq!(String::from("123.."), ensure_max_length(&original, 5));
-        assert_eq!(String::from("1234.."), ensure_max_length(&original, 6));
-        assert_eq!(String::from("12345.."), ensure_max_length(&original, 7));
-        assert_eq!(String::from("123456.."), ensure_max_length(&original, 8));
-        assert_eq!(original, ensure_max_length(&original, 9));
-        assert_eq!(original, ensure_max_length(&original, 10));
-        assert_eq!(original, ensure_max_length(&original, 20));
-    }
-}
+mod tests {}
