ab04ae8ed9d3b0f256cfdd67b65ad3879d08ab75
Artem Goncharov <Artem.goncharov@gmail.com>
2025-05-20 19:15:03 +0200

    feat: Prepare cli config for column magic (#1246)
    Extend the cli config to include column constraints for improving the
    output. For now they are not used.

--- openstack_cli/src/output.rs
+++ openstack_cli/src/output.rs
@@ -22,17 +22,65 @@ use std::io::{self, Write};
 
 use crate::OpenStackCliError;
 use crate::cli::{Cli, OutputFormat, TableArrangement};
-use structable::{OutputConfig, StructTable};
+use crate::config::{FieldConfig, ViewConfig};
+use structable::{OutputConfig, StructTable, StructTableOptions};
 
 /// Output Processor
 #[derive(Clone)]
 pub(crate) struct OutputProcessor {
-    /// Output configuration
-    pub(crate) config: OutputConfig,
+    /// Resource output configuration
+    pub(crate) config: Option<ViewConfig>,
     /// Whether output is for human or for machine
     pub(crate) target: OutputFor,
     /// Table arrangement
     pub(crate) table_arrangement: TableArrangement,
+    /// Fields requested
+    pub(crate) fields: BTreeSet<String>,
+    /// Wide mode
+    pub(crate) wide: bool,
+    /// Pretty mode
+    pub(crate) pretty: bool,
+}
+
+impl StructTableOptions for OutputProcessor {
+    fn wide_mode(&self) -> bool {
+        self.wide
+    }
+
+    fn pretty_mode(&self) -> bool {
+        self.pretty
+    }
+
+    fn should_return_field<S: AsRef<str>>(&self, field: S, is_wide_field: bool) -> bool {
+        let is_requested = self
+            .fields
+            .iter()
+            .any(|x| x.to_lowercase() == field.as_ref().to_lowercase())
+            || (self.fields.is_empty()
+                && self
+                    .config
+                    .as_ref()
+                    .map(|cfg| {
+                        cfg.fields.iter().any(|x| {
+                            match x {
+                                FieldConfig::Simple(name) => name,
+                                FieldConfig::Extended { name, .. } => name,
+                            }
+                            .to_lowercase()
+                                == field.as_ref().to_lowercase()
+                        })
+                    })
+                    .is_some_and(|x| x));
+
+        if !is_wide_field {
+            // Return non wide field when no field filters passed or explicitly requested the field
+            is_requested || (self.fields.is_empty() && self.config.is_none())
+        } else {
+            // The wide field is returned in wide mode when no filters passed or explicitly
+            // requested the field
+            (self.fields.is_empty() && self.wide_mode()) || is_requested
+        }
+    }
 }
 
 /// Output target (human or machine) enum
@@ -61,13 +109,12 @@ impl OutputProcessor {
             _ => OutputFor::Machine,
         };
         Self {
-            config: OutputConfig {
-                fields: BTreeSet::from_iter(args.global_opts.fields.iter().cloned()),
-                wide: matches!(args.global_opts.output, Some(OutputFormat::Wide)),
-                pretty: args.global_opts.pretty,
-            },
+            config: None,
             target,
             table_arrangement: args.global_opts.table_arrangement,
+            fields: BTreeSet::from_iter(args.global_opts.fields.iter().cloned()),
+            wide: matches!(args.global_opts.output, Some(OutputFormat::Wide)),
+            pretty: args.global_opts.pretty,
         }
     }
 
@@ -78,23 +125,14 @@ impl OutputProcessor {
             Some(OutputFormat::Wide) => OutputFor::Human,
             _ => OutputFor::Machine,
         };
-        let mut config: OutputConfig = OutputConfig {
+
+        Self {
+            config: args.config.views.get(resource_key.as_ref()).cloned(),
+            target,
+            table_arrangement: args.global_opts.table_arrangement,
             fields: BTreeSet::from_iter(args.global_opts.fields.iter().cloned()),
             wide: matches!(args.global_opts.output, Some(OutputFormat::Wide)),
             pretty: args.global_opts.pretty,
-        };
-        if config.fields.is_empty() && !config.wide {
-            if let Some(cfg) = args.config.views.get(resource_key.as_ref()) {
-                config = cfg.clone();
-                config.wide = matches!(args.global_opts.output, Some(OutputFormat::Wide));
-                config.pretty = args.global_opts.pretty;
-            }
-        }
-
-        Self {
-            config,
-            target,
-            table_arrangement: args.global_opts.table_arrangement,
         }
     }
 
@@ -127,8 +165,7 @@ impl OutputProcessor {
                         )
                     })?;
 
-                let (headers, table_rows) =
-                    structable::build_list_table(table.iter(), &self.config);
+                let (headers, table_rows) = structable::build_list_table(table.iter(), self);
                 let mut statuses: Vec<Option<String>> =
                     table.iter().map(|item| item.status()).collect();
 
@@ -204,12 +241,237 @@ impl OutputProcessor {
     /// Produce output for machine
     /// Return machine readable output with the API side names
     pub fn output_machine(&self, data: serde_json::Value) -> Result<(), OpenStackCliError> {
-        if self.config.pretty {
+        if self.pretty {
             serde_json::to_writer_pretty(io::stdout(), &data)?;
         } else {
             serde_json::to_writer(io::stdout(), &data)?;
         }
         io::stdout().write_all(b"\n")?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_field_returned_no_selection() {
+        let out = OutputProcessor {
+            config: None,
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::new(),
+            wide: false,
+            pretty: false,
+        };
+
+        assert!(
+            out.should_return_field("dummy", false),
+            "default field returned in non-wide mode with empty fields selector"
+        );
+        assert!(
+            !out.should_return_field("dummy", true),
+            "wide field not returned in non-wide mode with empty fields selector"
+        );
+
+        let out = OutputProcessor {
+            config: None,
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::new(),
+            wide: true,
+            pretty: false,
+        };
+
+        assert!(
+            out.should_return_field("dummy", false),
+            "default field returned in wide mode with empty fields selector"
+        );
+        assert!(
+            out.should_return_field("dummy", true),
+            "wide field returned in wide mode with empty fields selector"
+        );
+    }
+
+    #[test]
+    fn test_field_returned_selection_no_config() {
+        let out = OutputProcessor {
+            config: None,
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::from(["foo".to_string()]),
+            wide: false,
+            pretty: false,
+        };
+
+        assert!(
+            !out.should_return_field("dummy", false),
+            "default field not returned in non-wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("dummy", true),
+            "wide field not returned in non-wide mode with mismatching fields selector"
+        );
+        assert!(
+            out.should_return_field("foo", false),
+            "default field returned in non-wide mode with matching fields selector"
+        );
+        assert!(
+            out.should_return_field("foo", true),
+            "wide field returned in non-wide mode with matching fields selector"
+        );
+
+        let out = OutputProcessor {
+            config: None,
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::from(["foo".to_string()]),
+            wide: true,
+            pretty: false,
+        };
+
+        assert!(
+            !out.should_return_field("dummy", false),
+            "default field not returned in wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("dummy", true),
+            "wide field not returned in wide mode with mismatching fields selector"
+        );
+    }
+
+    #[test]
+    fn test_field_returned_selection_with_config_with_filters() {
+        let out = OutputProcessor {
+            config: Some(ViewConfig {
+                fields: Vec::from([FieldConfig::Simple("foo".to_string())]),
+            }),
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::from(["bar".to_string()]),
+            wide: false,
+            pretty: false,
+        };
+
+        assert!(
+            !out.should_return_field("dummy", false),
+            "default field not returned in non-wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("dummy", true),
+            "wide field not returned in non-wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("foo", false),
+            "default field not returned in non-wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("foo", true),
+            "wide field not returned in non-wide mode with mismatching fields selector"
+        );
+        assert!(
+            out.should_return_field("bar", false),
+            "default field returned in non-wide mode with matching fields selector"
+        );
+        assert!(
+            out.should_return_field("bar", true),
+            "wide field returned in non-wide mode with matching fields selector"
+        );
+
+        let out = OutputProcessor {
+            config: Some(ViewConfig {
+                fields: Vec::from([FieldConfig::Simple("foo".to_string())]),
+            }),
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::from(["bar".to_string()]),
+            wide: true,
+            pretty: false,
+        };
+
+        assert!(
+            !out.should_return_field("dummy", false),
+            "default field not returned in wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("dummy", true),
+            "wide field not returned in wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("foo", false),
+            "config field not returned in wide mode with mismatching fields selector"
+        );
+        assert!(
+            !out.should_return_field("foo", true),
+            "wide config field not returned in wide mode with mismatching fields selector"
+        );
+        assert!(
+            out.should_return_field("bar", false),
+            "default field returned in wide mode with matching fields selector"
+        );
+        assert!(
+            out.should_return_field("bar", true),
+            "wide field returned in wide mode with matching fields selector"
+        );
+    }
+
+    #[test]
+    fn test_field_returned_selection_with_config_no_filters() {
+        let out = OutputProcessor {
+            config: Some(ViewConfig {
+                fields: Vec::from([FieldConfig::Simple("foo".to_string())]),
+            }),
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::new(),
+            wide: false,
+            pretty: false,
+        };
+
+        assert!(
+            !out.should_return_field("dummy", false),
+            "default field not returned in non-wide mode with empty fields selector and not in config"
+        );
+        assert!(
+            out.should_return_field("foo", false),
+            "default field not returned in non-wide mode with empty fields selector, but in config"
+        );
+        assert!(
+            !out.should_return_field("dummy", true),
+            "wide field not returned in non-wide mode with empty fields selector and not in config"
+        );
+        assert!(
+            out.should_return_field("foo", true),
+            "wide field returned in non-wide mode with empty fields selector, but in config"
+        );
+
+        let out = OutputProcessor {
+            config: Some(ViewConfig {
+                fields: Vec::from([FieldConfig::Simple("foo".to_string())]),
+            }),
+            target: OutputFor::Human,
+            table_arrangement: TableArrangement::Disabled,
+            fields: BTreeSet::new(),
+            wide: true,
+            pretty: false,
+        };
+
+        assert!(
+            !out.should_return_field("dummy", false),
+            "default field not returned in wide mode with empty fields selector and not in config"
+        );
+        assert!(
+            out.should_return_field("foo", false),
+            "default field returned in wide mode with empty fields selector, but in config"
+        );
+        assert!(
+            out.should_return_field("dummy", true),
+            "wide field returned in wide mode with empty fields selector and not in config"
+        );
+        assert!(
+            out.should_return_field("foo", true),
+            "wide field returned in wide mode with empty fields selector, but in config"
+        );
     }
 }
