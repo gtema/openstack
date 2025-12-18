8bbc42a422b324d79f7f656fd918b4377f6e447f
Artem Goncharov <artem.goncharov@gmail.com>
2025-12-18 19:14:54 +0100

    chore: Apply new clippy fixes

--- openstack_tui/src/cloud_worker/compute/v2/aggregate/list.rs
+++ openstack_tui/src/cloud_worker/compute/v2/aggregate/list.rs
@@ -40,7 +40,7 @@ impl fmt::Display for ComputeAggregateList {
 
 impl TryFrom<&ComputeAggregateList> for RequestBuilder {
     type Error = Report;
-    fn try_from(value: &ComputeAggregateList) -> Result<Self, Self::Error> {
+    fn try_from(_value: &ComputeAggregateList) -> Result<Self, Self::Error> {
         let ep_builder = Self::default();
 
         Ok(ep_builder)
